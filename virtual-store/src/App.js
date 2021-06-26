52326747fa85382bca58b320e523e09c36578fda
micgreene <mgreene11111@yahoo.com>
2021-06-26 11:14:59 -0700

    created a new branch for lab 37

---
+++ virtual-store/src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/header/header.js';
+import Categories from './components/storefront/categories.js';
+import Footer from './components/footer/footer.js';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Categories />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ virtual-store/src/components/header/header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+
+  return (
+    <>
+      <header>
+        <h1>
+          OUR STORE
+        </h1>
+      </header>
+    </>
+  );
+
+}
+
+export default Header;
